1c8ca5064dc7500b6052bec827baf39c20a315cb
sofakob <hhitech628@gmail.com>
2025-10-26 12:52:49 +0200

    first commit

---
+++ prostechislo.py
@@ -0,0 +1,38 @@
+from test import miller_rabin, trial_division
+from l20 import l20
+
+
+def prostechislo(k:int):
+    test=False
+    while test==False:
+        p=l20(k)
+        proverochka=trial_division(p)
+        if proverochka==True:
+            test=miller_rabin(p)
+    return p
+
+
+
+
+
+def parochki():
+    k=256
+    q=prostechislo(k)
+    p=prostechislo(k)
+    q1=prostechislo(k)
+    p1=prostechislo(k)
+    qint=(int(''.join(str(i) for i in q), 2))
+    pint=(int(''.join(str(i) for i in p), 2))
+    q1int=(int(''.join(str(i) for i in q1), 2))
+    p1int=(int(''.join(str(i) for i in p1), 2))
+    if qint*pint<=q1int*p1int:
+        return q, p, q1, p1
+    else:
+        return q1, p1, q, p
+
+
+#print(int(''.join(str(i) for i in prostechislo(32)), 2))
+
+print(parochki())
+
+
---
+++ test.py
@@ -0,0 +1,64 @@
+from decimal import Decimal
+from math import gcd
+from random import randint
+from labanta import r_i_sequence
+
+
+
+def miller_rabin(a:list):
+    k=10
+    p=int(''.join(str(i) for i in a), 2)
+    a=Decimal(int(''.join(str(i) for i in a), 2))
+    p_1=a-Decimal(1)
+    s=0
+    d=p_1
+    psevdosila=True
+    while int(d)%2==0:
+        s+=1
+        d=d/Decimal(2)
+    for i in range(k):
+        x=randint(2, int(a)-1)
+        gcdx=gcd(x, p)
+        if gcdx>1:
+            return False
+        if pow(x, int(d), int(a))==1 or pow(x, int(d), int(a))==p-1:
+            psevdosila=True
+        else:
+            for i in range(s):
+                x=pow(x, 2, p)
+                if x==-1:
+                    psevdosila=True
+                    break
+                elif x==1:
+                    psevdosila=False
+                    break
+        if psevdosila==False:
+            return False
+    
+
+    return True
+
+
+def trial_division(n:list):
+    
+    n=Decimal(int(''.join(str(i) for i in n), 2))
+    
+    prime =  [Decimal(2), Decimal(3), Decimal(5), Decimal(7), Decimal(11), Decimal(13), Decimal(17), Decimal(19), Decimal(23), Decimal(29), Decimal(31), Decimal(37), Decimal(41), Decimal(43), Decimal(47)]
+
+    n_digits = [int(d) for d in str(n)]
+    a_i = n_digits[::-1] #список з цифр n в оберненому порядку
+
+    
+    for p in prime:       
+        r_i_p_seq = r_i_sequence(p, len(a_i)) #послідовність r_i для р
+
+        sum_ = 0 #сума для а_i * r_i
+         
+        for i, a in enumerate(a_i):
+            sum_ += a * r_i_p_seq[i]
+
+        if sum_ % p == 0:
+            return False
+
+    return True
+
