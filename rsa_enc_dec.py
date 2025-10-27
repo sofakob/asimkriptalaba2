540f4937be38f339024ec5e627017d93ab8a6ec8
Ivanna Navrotska <navrockaivanna15@gmail.com>
2025-10-27 08:36:13 +0200

    add rsa encrypt and decrypt

---
+++ rsa_enc_dec.py
@@ -0,0 +1,50 @@
+def Encrypt():
+    
+    message = input('Введіть назву файла з повідомленням, яке хочете зашифрувати: ')
+    public_key = input('Введіть назву файла з відкритим ключем адресата: ')
+
+    with open(message, 'r') as f:
+        m = int(f.read())
+
+    with open(public_key, 'r') as f:
+        n_e = f.readlines()
+
+    n = int(n_e[0].strip())
+    e = int(n_e[1].strip())
+
+    c = pow(m, e, n)
+
+    with open('cipher_text.txt', 'w') as f:
+        f.write(str(c))
+
+    print('Ваш шифротекст знаходиться у файлі cipher_text.txt')
+
+
+
+
+def Decrypt():
+
+    cipher_txt = input('Введіть назву файла з шифротекстом, який хочете зашифрувати: ')
+    private_key = input('Введіть назву файла зі своїм особистим ключем: ')
+
+    with open(cipher_txt, 'r') as f:
+        c = int(f.read())
+
+    with open(private_key, 'r') as f:
+        d_p_q = f.readlines()
+
+    d = int(d_p_q[0].strip())
+    p = int(d_p_q[1].strip())
+    q = int(d_p_q[2].strip())
+
+    n = p*q
+    
+    m = pow(c, d, n)
+
+    with open('message.txt', 'w') as f:
+        f.write(str(m))
+
+    print('Ваше повідомлення знаходиться у файлі message.txt')
+
+
+Decrypt()   
